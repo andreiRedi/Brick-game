0c5676d14a4a5afd09b17bf390aeb0a8e54e13da
andreiRedi <andrei.redi@gmail.com>
2023-03-24 15:57:55 +0200

    Ball added, bounces from walls and game over if touches floor

--- src/main.js
+++ src/main.js
@@ -1,22 +1,61 @@
-const board = document.querySelector(".game");
+class Ball {
+  constructor(board) {
+    this.board = board.dimension();
+    this.ball = document.querySelector(".circle1");
 
+    this.ball.style.left = `${Math.ceil(this.board.right / 2)}px`;
+    this.ball.style.top = `${Math.ceil(this.board.bottom) - 100}px`;
+
+    this.dx = 2;
+    this.dy = -2;
+  }
+
+  draw() {
+    const { style } = this.ball;
+    let { x, y } = this.dimension();
+    let radius = this.dimension().width / 2;
+
+    if (x + this.dx > this.board.width - radius || x + this.dx < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (y + this.dy < 0) {
+      this.dy = -this.dy;
+    }
+
+    if (y + this.dy > this.board.height - radius) {
+      alert("Game over");
+      window.cancelAnimationFrame();
+    }
+    style.left = `${x + this.dx}px`;
+    style.top = `${y + this.dy}px`;
+  }
+
+  dimension() {
+    return this.ball.getBoundingClientRect();
+  }
+}
 
 class Platform {
-  constructor(board) {
+  constructor(board, platform) {
     this.rightPressed = false;
     this.leftPressed = false;
 
+    this.platform = platform;
     this.board = board;
     this.platform = document.getElementById("platform");
+
+    document.addEventListener("keydown", (e) => this.keyDownHandler(e), false);
+    document.addEventListener("keyup", (e) => this.keyUpHandler(e), false);
   }
 
   draw() {
     let left = this.platform.style.left;
     let number = +left.slice(0, -2);
 
-    let boardSize = this.board.getBoundingClientRect();
+    let boardSize = this.board.dimension();
     let platformSize = this.platform.getBoundingClientRect();
-    
+
     if (this.rightPressed && platformSize.right <= boardSize.right) {
       this.platform.style.left = `${number + 5}px`;
     } else if (this.leftPressed && platformSize.left >= boardSize.left) {
@@ -42,24 +81,27 @@ class Platform {
 }
 
 class Game {
-  constructor(board) {
-    this.board = board;
-    this.height = board.height;
-    this.width = board.width;
+  constructor() {
+    this.board = document.querySelector(".game");
+    this.height = this.board.height;
+    this.width = this.board.width;
 
     this.platform = new Platform(this);
+    this.ball = new Ball(this, this.platform);
   }
 
   render() {
-    platform.draw();
+    this.ball.draw();
+    this.platform.draw();
+
     window.requestAnimationFrame(() => this.render());
+  }
+
+  dimension() {
+    return this.board.getBoundingClientRect();
   }
 }
 
-const game = new Game(board);
-const platform = new Platform(board);
-
-document.addEventListener("keydown", (e) => platform.keyDownHandler(e), false);
-document.addEventListener("keyup", (e) => platform.keyUpHandler(e), false);
+const game = new Game();
 
 game.render();
