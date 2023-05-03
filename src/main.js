425642e7a07bbc7e17f61811ca785cc02f7d8fdc
laagusra <rain.laagus@gmail.com>
2023-05-03 16:53:01 +0300

    bricks and collision

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const ballHTML = document.querySelector(".circle1");
 const boardHTML = document.querySelector(".game");
 const platformHTML = document.getElementById("platform");
 // test
-const ballDirection = { dx: 1, dy: -1 };
+const ballDirection = { dx: 2, dy: -2 };
 const keys = { rightPressed: false, leftPressed: false };
 
 const getDimensions = () => {
@@ -31,6 +31,9 @@ const keyDownHandler = (e) => {
     keys.rightPressed = true;
   } else if (e.key == "Left" || e.key == "ArrowLeft") {
     keys.leftPressed = true;
+  } else if (e.key == "Escape") {
+    alert("Escape")
+    window.cancelAnimationFrame(renderGame);
   }
 };
 
@@ -65,10 +68,9 @@ const drawBall = () => {
   } else if (bottom + dy > platform.top) {
     if (x > platform.x && x < platform.x + platform.width) {
       ballDirection.dy = -dy;
-    }  else if (bottom + dy > board.bottom) {
-      alert("Game over");
+    } else if (bottom + dy > board.bottom) {
       window.cancelAnimationFrame(renderGame);
-      return;
+      return
     }
   }
 
@@ -89,12 +91,48 @@ const drawPlatform = () => {
   }
 };
 
+const build = () => {
+  let brickIndex = 0
+  for (let index = 0; index < 9; index++) {
+    let element = document.createElement('div')
+    element.setAttribute("id", "brickRow" + index)
+    element.classList.add('brickRow');
+    document.getElementById("game").appendChild(element)
+    for (let index2 = 0; index2 < 4; index2++) {
+      let element2 = document.createElement('div')
+      element2.setAttribute("id", "brick-" + brickIndex++)
+      element2.classList.add('brick');
+      element2.innerHTML = "visible"
+      element.appendChild(element2)
+    }
+  }
+}
+
+function collisionDetection() {
+  let bricks = document.getElementsByClassName("brick")
+  const { ball } = getDimensions();
+  const { dy } = ballDirection;
+  const { x, y } = ball;
+  for (let c = 0; c < bricks.length; c++) {
+    let brick = bricks[c].getBoundingClientRect()
+    if (x > brick.x && x < brick.x + brick.width && y > brick.y && y < brick.y + brick.height && bricks[c].innerHTML === "visible") {
+      bricks[c].innerHTML = "hidden"
+      bricks[c].style.opacity = "0"
+      ballDirection.dy = -dy;
+    }
+  }
+}
+
 const renderGame = () => {
   drawBall();
   drawPlatform();
+  collisionDetection()
+
   window.requestAnimationFrame(() => renderGame());
 };
 
 initialize();
+build();
+
 
 renderGame();
